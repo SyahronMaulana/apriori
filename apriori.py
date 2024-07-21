30ba4418d8db9ad7b0be38a6e70d0025424c0518
SyahronMaulana <syahronmaulana123@gmail.com>
2024-07-21 16:51:16 +0700

    python website

---
+++ apriori.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from mlxtend.frequent_patterns import association_rules, apriori
+
+df = pd.read_csv("data-transaksi2.csv")
+df['date_time'] = pd.to_datetime(df['date_time'], format='%Y-%m-%d')
+
+df['year_month_day'] = df['date_time'].dt.to_period('D')
+
+df['year_month_day'].replace([i for i in range(1, 12 +1)], ["januari", "februari", "maret", "april", "mei", "juni", "juli", "agustus", "september", "oktober", "november", "desember"])
+
+st.title("Apriori")
